3bee3a6d1fd337a6a2bf9586b71b66704c02f070
felixd3v <fmhcde@gmail.com>
2022-03-14 11:03:57 -0500

    new commit

---
+++ pattern-matching/pattern.exs
@@ -0,0 +1,7 @@
+case {1, 2, 3} do
+  {1, 2, number } ->
+    IO.puts number
+    _ ->
+      IO.puts "No match"
+
+end
---
+++ pattern-matching/pattern2.exs
@@ -0,0 +1,5 @@
+map = %{golf: "UK", soccer: "Mexico", hockey: "Canada"}
+%{golf: country1, soccer: country2, hockey: country3} = map
+IO.puts country1
+IO.puts country2
+IO.puts country3
